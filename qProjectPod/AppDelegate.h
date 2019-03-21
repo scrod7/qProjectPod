56974e0767bdf0c15c12bd5f57b61b9c5eb0eb69
xueyu7 <xueyuqi_tju@163.com>
2019-03-21 11:43:24 +0800

    first commit

---
+++ qProjectPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qProjectPod
+//
+//  Created by xueyuqi on 2019/3/21.
+//  Copyright © 2019 xueyuqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qProjectPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qProjectPod
+//
+//  Created by xueyuqi on 2019/3/21.
+//  Copyright © 2019 xueyuqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
